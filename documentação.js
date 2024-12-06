3de0565dc0d770962614e8f87dd7df4a5a36c7de
euKaKau <kauanetamara0003@gmail.com>
2024-12-06 13:23:38 -0300

    Add files via upload

---
+++ documentação.js
@@ -0,0 +1,4 @@
+DOCUMENTAÇÃO
+
+//Este projeto foi muito desafiador, pois deu muitos erros no github e como também no VSCODE, em alguns momentos foi recorrido aos meus colegas (que estavam sofrendo com os mesmos problemas, pois não tínhamos informações suficientes para construir o projeto).
+//Após o professor explicar novamente e de uma forma, muito mais clara, consegui resolver, mas com algumas dificuldades, que com ajuda da internet foi possível solucionar o problema.
